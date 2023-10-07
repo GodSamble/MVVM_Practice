fb834b8c912afdd1214a30e9660e346bbebfe1ae
GodSamble <osgneb8359@naver.com>
2023-10-08 02:29:22 +0900

    [Feat, API] MVVM 패턴, API통신(Get)

---
+++ MVVM_Practice/View/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ArticleTableViewCell.swift
+//  MVVM_Practice
+//
+//  Created by 고영민 on 10/8/23.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
